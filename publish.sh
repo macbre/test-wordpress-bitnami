d2a25c1658e29c6e8593603e42cf9032257020f2
macbre <maciej.brencz@gmail.com>
2023-03-02 14:01:34 +0000

    publish.sh: make a random picsum.photos image a featured one for the published post
    <13>Mar  2 13:59:44 wp-publish[663545]: Uploading an image from <https://fastly.picsum.photos/id/957/1280/500.jpg?hmac=1acflVToLySh6RfNyccWlS3wZLvmE0WqgMbjGNsLJk0> and making it a featured image for #1043 post ...
    Imported file 'https://fastly.picsum.photos/id/957/1280/500.jpg?hmac=1acflVToLySh6RfNyccWlS3wZLvmE0WqgMbjGNsLJk0' as attachment ID 1044 and attached to post 1043 as featured image.
    Success: Imported 1 of 1 images.
    <13>Mar  2 13:59:49 wp-publish[663597]: Done

--- publish.sh
+++ publish.sh
@@ -20,8 +20,23 @@ log "Will post the following content as '${TITLE}': ${CONTENT}"
 set -x
 
 # -T disable pseudo-tty allocation
+# https://developer.wordpress.org/cli/commands/post/create/
 docker-compose -f ${DIRECTORY}/docker-compose.yml exec -T wordpress \
-	wp post create --post_title="${TITLE}" --post_content="${CONTENT}" --post_status='publish'
+	wp post create --post_title="${TITLE}" --post_content="${CONTENT}" --post_status='publish' --porcelain | tee /tmp/post_id
 
 set +x
+
+POST_ID="$(cat /tmp/post_id)"
+
+# attach a photo to the new post
+# e.g. https://fastly.picsum.photos/id/828/450/150.jpg?hmac=AcD5dfz3UrPo15KUhXFzaxSWBrL7qQ13vbXWZsEaSAE
+PHOTO_URL="$(curl -v 'https://picsum.photos/1280/500' 2>&1 | grep -i 'location:' | grep -Eo 'https[A-Za-z:/.0-9?=+-]+')"
+
+log "Uploading an image from <${PHOTO_URL}> and making it a featured image for #${POST_ID} post ..."
+
+# https://developer.wordpress.org/cli/commands/media/import/
+docker-compose -f ${DIRECTORY}/docker-compose.yml exec -T wordpress \
+	wp media import "${PHOTO_URL}" \
+		--title="Image" --caption="An image" --featured_image --post_id=${POST_ID} \
+
 log "Done"
